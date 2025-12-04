b138fbfaf42c5ac6d2fe66132e2911052fca6647
Kilian <dankestmirror@hotmail.es>
2025-12-04 17:51:54 +0000

    Header Style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,14 @@
 <script setup>
-import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute} from 'vue-router';
+import { computed } from 'vue';
+    const route = useRoute()
+    const paginaInicio = computed(() => route.name === 'inicio')
 </script>
+
 <template>
-    <header class="bg-slate-800">
+    <header class="bg-slate-800"
+            :class="{header : paginaInicio}"
+    >
         <div class=" mx-auto container px-5 py-16">
             <div class=" flex justify-between items-center">
                 <div>
@@ -10,49 +16,63 @@ import { RouterLink } from 'vue-router';
                         <img class=" w-32" src="/img/logo.svg" alt="Logo">
                     </RouterLink>
                 </div>
+                
                 <nav class=" flex gap-4">
                     <RouterLink
-                        to="{name: 'inicio'}"  
+                        :to="{name: 'inicio'}"  
                         class="text-white uppercase font-bold"   
-                        active-class="active-Router"                
+                        active-class="linkActive-Orange"                
                     >Inicio</RouterLink>
+                    
                     <RouterLink
                         :to="{name: 'favoritos'}"  
                         class="text-white uppercase font-bold"
-                        active-class="text-orange-500"
-                        
+                        active-class="linkActive-Orange"
                     >Favoritos</RouterLink>
                 </nav>
             </div>
         
-        <form action=""
-            class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"        
-        >
-            <div class="space-y-4">
-                <label class=" block text-white uppercase font-extrabold text-lg"
-                for="ingrediente">Nombre o Ingredientes</label>
-                <input 
-                id="ingredientes"
-                type="text"
-                class=" p-3 w-full rounded-lg focus:outline-none bg-white"
-                placeholder="Nombre o Ingredientes: ej. Vodka, Tequila, Etc"
+            <form action=""
+                class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"        
+                v-if="paginaInicio"            
                 >
-            </div>
-            <div class="space-y-4">
-                <label class=" block text-white uppercase font-extrabold text-lg"
-                for="categoria">Categoría</label>
-                <select
-                id="categoria"
-                class=" p-3 w-full rounded-lg focus:outline-none bg-white">
-                    <option value="">-- Seleccione --</option>
-                </select>
-            </div>
-    
-            <input type="submit"
-            class=" bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase"
-            value="Buscar Recetas"
-            >
-    </form>
-    </div>
+                <div class="space-y-4">
+                    <label class=" block text-white uppercase font-extrabold text-lg"
+                    for="ingrediente">Nombre o Ingredientes</label>
+                    <input 
+                    id="ingredientes"
+                    type="text"
+                    class=" p-3 w-full rounded-lg focus:outline-none bg-white"
+                    placeholder="Nombre o Ingredientes: ej. Vodka, Tequila, Etc"
+                    >
+                </div>
+                <div class="space-y-4">
+                    <label class=" block text-white uppercase font-extrabold text-lg"
+                    for="categoria">Categoría</label>
+                    <select
+                    id="categoria"
+                    class=" p-3 w-full rounded-lg focus:outline-none bg-white">
+                        <option value="">-- Seleccione --</option>
+                    </select>
+                </div>
+        
+                <input type="submit"
+                class=" bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase"
+                value="Buscar Recetas"
+                >
+            </form>
+        </div>
     </header>
 </template>
+
+<style scoped>
+
+.linkActive-Orange {
+    color: #f97316 !important;
+}
+.header {
+    background-image: url('/img/bg.jpg');
+    background-position: center;
+    background-size: cover;
+}
+</style>
